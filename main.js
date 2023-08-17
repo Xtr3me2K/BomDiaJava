7a8127fffed4b38244774e98f13c1ec646860d38
Xtr3me2K <130668153+Xtr3me2K@users.noreply.github.com>
2023-08-17 09:29:44 -0300

    Commit 2

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-let saudacao = "Bom dia!"
+let saudacao = "";
+const horaAtual = new Date().getHours();
+const imagem = document.getElementById("imagem");
 
-console.log(saudacao)
+if (horaAtual >= 6 && horaAtual < 12) {
+    saudacao = "Bom dia!";
+} else if (horaAtual >= 12 && horaAtual < 18) {
+    saudacao = "Boa tarde!";
+} else {
+    saudacao = "Boa noite!";
+}
 
-document.getElementById("resultado").innerHTML = saudacao
+document.getElementById("resultado").textContent = saudacao;
